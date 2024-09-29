4113a21b170e1749effe8f1c4baeab71118676de
Pedro Bueno <pedroarcbueno@gmail.com>
2024-09-28 21:23:56 -0300

    feat: Listview and NoteDetailView

---
+++ FastNote/FastNote/Pages/NoteList/NoteDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  NoteDetailView.swift
+//  FastNote
+//
+//  Created by pedro.bueno on 28/09/24.
+//
+
+import SwiftUI
+
+struct NoteDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NoteDetailView()
+}
---
+++ FastNote/FastNote/Pages/NoteList/NoteListViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  NoteListViewModel.swift
+//  FastNote
+//
+//  Created by pedro.bueno on 28/09/24.
+//
+
+import Foundation
